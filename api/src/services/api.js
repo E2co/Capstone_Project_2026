3377ff31f9cb1a251f2e599766a35b9e35d90c6d
danielle-brooks <danielle04brooks@gmail.com>
2026-04-23 10:59:18 -0500

    Front end

---
+++ api/src/services/api.js
@@ -0,0 +1,15 @@
+import axios from "axios";
+
+const API = axios.create({
+  baseURL: "http://127.0.0.1:8000",
+});
+
+export const getTransactions = (limit = 10) => API.get(`/transactions/?limit=${limit}`);
+
+export const assessTransaction = (data) => API.post("/assess_transaction/", data);
+
+export const getSettings = () => API.get("/settings/");
+
+export const updateSettings = (data) => API.post("/settings/", data);
+
+export const getAnalytics = () => API.get("/analytics/");
